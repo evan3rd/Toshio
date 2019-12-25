186a646e7fd12b2890883f9bc836f3528db913ca
Evan.Wu <evan@gmail.com>
2019-12-25 14:08:43 +0800

    project init

---
+++ Toshio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Toshio
+//
+//  Created by evanwu01 on 2019/12/25.
+//  Copyright © 2019 ShrimpWangao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
